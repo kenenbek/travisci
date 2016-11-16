fe8eb3eebec5d4ac8080c49f40d1372525b34113
Ken <kenenbek>
2016-11-16 16:56:40 +0300

    install simgrid in travis. checking

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <simgrid/msg.h>
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(msg_app_masterworker, "Messages specific for this msg example");
 
 int main() {
-    std::cout << "Hello, Worlld!" << std::endl;
+    XBT_INFO("Hello, simgrid");
     return 0;
 }
